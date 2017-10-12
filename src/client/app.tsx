a196294e68b47576fddfdcf587627e21cc3bdc37
rdoo <rafdomaradzki@gmail.com>
2017-10-12 23:35:21 +0200

    options component scaffold

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 
 import { Config } from '../ml/config';
-import { NetworkSerialized, SpeciesSerialized, StateSerialized } from '../ml/serialization.models';
+import { NetworkSerialized, StateSerialized } from '../ml/serialization.models';
 import { CanvasComponent } from './canvas';
 import { CheckComponent } from './check';
+import { OptionsComponent } from './options';
 
 interface InputData {
     [key: string]: any[];
@@ -78,7 +79,7 @@ export class App extends React.Component {
         this.ws.send('DA' + dataName);
     }
 
-    showNetwork(network: NetworkSerialized) {
+    openCheckComponent(network: NetworkSerialized) {
         this.setState({ chosenNetwork: network });
     }
 
@@ -108,6 +109,7 @@ export class App extends React.Component {
                 <button disabled={this.state && this.state.running} onClick={() => this.start()}>START</button>
                 <button disabled={this.state && !this.state.running} onClick={() => this.pause()}>{this.state && this.state.paused ? 'UNPAUSE' : 'PAUSE'}</button>
                 <button disabled={this.state && !this.state.running} onClick={() => this.stop()}>STOP</button>
+                <button>OPTS</button>
                 Networks #: <input disabled={this.state && this.state.running} defaultValue={String(this.config.networksNumber)} onKeyUp={event => this.config.networksNumber = Number((event.target as HTMLInputElement).value)} />
                 Culling %: <input disabled={this.state && this.state.running} defaultValue={String(this.config.cullingPercent)} onKeyUp={event => this.config.cullingPercent = Number((event.target as HTMLInputElement).value)} />
                 Fitness threshold: <input disabled={this.state && this.state.running} defaultValue={String(this.config.fitnessThreshold)} onKeyUp={event => this.config.fitnessThreshold = Number((event.target as HTMLInputElement).value)} />
@@ -118,11 +120,12 @@ export class App extends React.Component {
                 C2: <input disabled={this.state && this.state.running} defaultValue={String(this.config.c2)} onKeyUp={event => this.config.c2 = Number((event.target as HTMLInputElement).value)} />
                 C3: <input disabled={this.state && this.state.running} defaultValue={String(this.config.c3)} onKeyUp={event => this.config.c3 = Number((event.target as HTMLInputElement).value)} />
                 Species threshold: <input disabled={this.state && this.state.running} defaultValue={String(this.config.sameSpeciesThreshold)} onKeyUp={event => this.config.sameSpeciesThreshold = Number((event.target as HTMLInputElement).value)} />
+                <OptionsComponent></OptionsComponent>
                 {this.state && this.state.mlState && <div>
                     <div>Current step: {this.state.mlState.step}</div>
                     <div>Species: {this.state.mlState.speciesArray.length} ({this.state.mlState.speciesArray.reduce((a, b) => a + b.networks.length, 0)})</div>
                     {this.state.mlState.speciesArray.map((species, i) => <div key={i}>{i} - {species.networks.length} - {species.desiredPopulation} - {species.averageFitness} - {species.networks[0].fitness}</div>)}
-                    <button onClick={() => this.showNetwork(this.state.mlState.bestNetwork)}>SHOW</button>
+                    <button onClick={() => this.openCheckComponent(this.state.mlState.bestNetwork)}>SHOW</button>
                     <span>Best fitness: {this.state.mlState.bestNetwork.fitness}</span>
                     <CanvasComponent network={this.state.mlState.bestNetwork}></CanvasComponent>
                 </div>}
@@ -133,7 +136,7 @@ export class App extends React.Component {
                                 <input value={String(this.state.currentlyViewed[i])} onChange={event => this.setCurrentlyViewed(i, Number((event.target as HTMLInputElement).value))}/>
                                 <button disabled={this.state.currentlyViewed[i] <= 0} onClick={() => this.setCurrentlyViewed(i, this.state.currentlyViewed[i] - 1)}>PREV</button>
                                 <button disabled={this.state.currentlyViewed[i] >= (species.networks.length - 1)} onClick={() => this.setCurrentlyViewed(i, this.state.currentlyViewed[i] + 1)}>NEXT</button>
-                                <button onClick={() => this.showNetwork(species.networks[this.state.currentlyViewed[i]])}>SHOW</button>
+                                <button onClick={() => this.openCheckComponent(species.networks[this.state.currentlyViewed[i]])}>SHOW</button>
                                 Actual networks: {species.networks.length} Desired: {species.desiredPopulation} Avg fitness: {species.averageFitness} This fitness: {species.networks[this.state.currentlyViewed[i]].fitness}
                             </div>
                             <CanvasComponent network={species.networks[this.state.currentlyViewed[i]]}></CanvasComponent>
